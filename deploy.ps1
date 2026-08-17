1d6d0c9df63b054e7439d54b4a50a5ab436bc0a9
charliekim1002 <kimcharlie1002@gmail.com>
2026-08-18 01:16:55 +0900

    Update portfolio 2026-08-18 01:16

--- deploy.ps1
+++ deploy.ps1
@@ -1,3 +1,8 @@
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+[Console]::InputEncoding  = [System.Text.Encoding]::UTF8
+$OutputEncoding = [System.Text.Encoding]::UTF8
+chcp 65001 | Out-Null
+
 $ErrorActionPreference = "Stop"
 
 # ==========================================
